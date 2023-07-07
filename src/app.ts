2fb6b062e2960535ac1794a0db690a9f4eb8bb11
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-07 20:49:46 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,106 +1,209 @@
-// const button = document.querySelector("button");
-// const input1 = document.getElementById("num1")! as HTMLInputElement;
-// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// class House {
+//   private tenats: string[] = [];
 
-// function add(num1: number, num2: number) {
-//   return num1 + num2;
+//   constructor(public readonly type: string, protected street: string) {}
+
+//   public showAddress(this: House): void {
+//     console.log('Address: ' + this.street);
+//   }
+
+//   public showType(this: House): void {
+//     console.log('Address: ' + this.type);
+//   }
+
+//   public addTenant(name: string) {
+//     this.tenats.push(name);
+//   }
+
+//   public showTenants() {
+//     console.log(this.tenats);
+//   }
 // }
 
-// button.addEventListener("click", function() {
-//   console.log(add(+input1.value, +input2.value));
-// });
+// class StoneHouse extends House {
+//   private chatgeOfTheHouse: string;
 
-// let age:number ;
-//   age = 50;
-// let name:string;
-// name = 'Max';
-// let toggle: boolean;
-//   toggle = true;
-// let empty: null;
-//   empty = null;
-// let notInitialize: undefined;
-//   notInitialize = undefined;
-// let callback: (a:number)=> number;
-//   callback = a => {
-//   return 100 + a;
+//   constructor(street: string, general: string) {
+//     super('stone', street);
+//     this.chatgeOfTheHouse = general;
+//   }
+
+//   public showTenants() {
+//     console.log('General: ' + this.chatgeOfTheHouse);
+
+//     super.showTenants();
+//   }
+
+//   public showAddress(): void {
+//     console.log('Address: ' + this.street);
+//   }
+// }
+
+// const stoneHouse = new StoneHouse('Stone-world', 'Max');
+// stoneHouse.addTenant('Karl');
+// stoneHouse.addTenant('Vus');
+
+// stoneHouse.showTenants();
+// stoneHouse.showAddress();
+
+// class UseStatic {
+//   private static count = 0;
+
+//   constructor() {
+//     UseStatic.count += 1;
+//   }
+
+//   public static isStaticMethod() {
+//     console.log("I'm static");
+//   }
+
+//   public showCount() {
+//     console.log(UseStatic.count);
+//   }
+// }
+
+// const obj1 = new UseStatic();
+// const obj2 = new UseStatic();
+// const obj3 = new UseStatic();
+
+// obj1.showCount();
+// obj2.showCount();
+// obj3.showCount();
+
+// UseStatic.isStaticMethod();
+
+//абстракція
+
+// abstract class Plane {
+//   protected pilotInCabin = false;
+
+//   public sitInPlane() {
+//     this.pilotInCabin = true;
+//   }
+
+//   public abstract startEngine(): string;
+// }
+
+// class Maize extends Plane {
+//   public startEngine() {
+//     return 'Maize engine started';
+//   }
+// }
+
+// class Boing extends Plane {
+//   public startEngine() {
+//     return 'Boing engine started';
+//   }
+// }
+
+// const maize = new Maize();
+// const boing = new Boing();
+
+// maize.sitInPlane();
+// boing.sitInPlane();
+
+// console.log(maize.startEngine());
+// console.log(boing.startEngine());
+
+//інтерфейси
+
+// interface IPerson {
+//   readonly name: string;
+//   readonly age?: number;
+
+//   greet(phrase: string): void;
+// }
+
+// // type IPerson = {
+// //   readonly name: string;
+// //   age: number;
+
+// //   greet: (phrase: string) => void;
+// // };
+
+// let user: IPerson;
+// user = {
+//   name: 'John',
+//   age: 40,
+//   greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name);
+//   },
 // };
 
-// let anything: any;
-// anything = -20;
-// anything = 'Text';
-// anything = {};
+// user.greet('Hi, there. My name is');
 
-// let some:unknown;
-// some = 'Text';
+//інтерфейси class
 
+// interface IPerson {
+//   readonly name: string;
+//   age?: number;
 
-// let str: string;
-
-// if(typeof some === 'string' )
-
-// str = some;
-
-// let person: [string, number];
-// person = ['Max', 21];
-
-// enum Load { LOADING, READY };
-
-// const page = {
-//   load: Load.READY
+//   greet(phrase: string): void;
+// }
+// interface IPilot {
+//   flyMessage(): void;
 // }
 
-// if (page.load === Load.LOADING) {
-//   console.log('Loading...');
+// class Pilot implements IPerson, IPilot {
+//   constructor(public readonly name: string, public age: number) {
+//     this.checkAge;
+//   }
+//   private checkAge() {
+//     if (this.age < 28) {
+//       throw new Error('Pilot is to young');
+//     }
+//   }
+//   public greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name);
+//   }
+
+//   public flyMessage(): void {
+//     console.log('The plane took height. Enjoy your fly.');
+//   }
 // }
 
-// if (page.load === Load.READY) {
-//   console.log('Ready');
+// abstract class Plane {
+//   protected pilot?: IPilot;
+
+//   public sitInPlane(pilot: IPilot) {
+//     this.pilot = pilot;
+//   }
+
+//   public abstract startEngine(): boolean;
 // }
 
-let solution: string | number;
-let butB: 'anable' | 'disable';
+// class Boeing extends Plane {
+//   public startEngine() {
+//     if (!this.pilot) {
+//       throw new Error('No pilot in the boat');
+//     }
+//     console.log('Boeing engine started');
+//     pilot.flyMessage();
+//     return true;
+//   }
+// }
 
+// const pilot = new Pilot('Ali', 25);
+// const boeing = new Boeing();
+// pilot.greet('Hi, there. My name is');
 
+// boeing.sitInPlane(pilot);
+// boeing.startEngine();
 
+//type function
 
-function showMessage(message:string):void {
-  console.log(message);
-}
+// type AddFunc = (n1: number, n2: number) => number;
 
+// const foo: AddFunc = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
 
-function calc(num1:number, num2:number): number {
-  return num1 + num2;
-}
+// interface AddFunc {
+//   (n1: number, n2: number): number;
+// }
 
-function customError():never {
-  throw new Error('Error');
-}
+// const foo: AddFunc = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
 
-type PageType = {
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: string,
-  details?: {
-    createAt: Date,
-    updateAt: Date
-  }
-}
-
-const page1: PageType = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
-
-const page2: PageType = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+//5.
