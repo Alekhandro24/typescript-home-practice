ea45db6a87ecd1346ce04ff40185d6dc560168ca
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-09 12:46:52 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -474,14 +474,316 @@
 // };
 
 //Pick
-interface Page {
-  title: string;
-  annotation: string;
-  numberPage: number;
+// interface Page {
+//   title: string;
+//   annotation: string;
+//   numberPage: number;
+// }
+
+// const pageAnnotation: Pick<Page, 'annotation' | 'numberPage'> = {
+//   annotation: 'Small page',
+//   numberPage: 1,
+// };
+// console.log('pageAnnotation: ', pageAnnotation);
+
+//v1
+// type Admin = {
+//   name: string;
+//   privilages: string[];
+// };
+
+// type User = {
+//   name: string;
+//   startDate: Date;
+// };
+
+// type AdminUser = Admin & User;
+// const user: AdminUser = {}
+
+interface Admin {
+  name: string;
+  privilages: string[];
 }
 
-const pageAnnotation: Pick<Page, 'annotation' | 'numberPage'> = {
-  annotation: 'Small page',
-  numberPage: 1,
-};
-console.log('pageAnnotation: ', pageAnnotation);
+interface User {
+  name: string;
+  startDate: Date;
+}
+// interface AdminUser extends Admin, User {}
+
+// type ComplexType = string | number;
+
+// function combine(a: ComplexType, b: ComplexType) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+
+// type AdminUser = Admin | User;
+
+// function showFields(el: AdminUser) {
+//   if ('startDate' in el) {
+//     console.log('el.startDate: ', el.startDate);
+//   }
+//   if ('privilages' in el) {
+//     console.log('el.privilages: ', el.privilages);
+//   }
+//   console.log('el.name: ', el.name);
+// }
+
+// const user: Admin = {
+//   name: 'Ali',
+//   // startDate: new Date(),
+//   privilages: ['create', 'read', 'update', 'delete'],
+// };
+
+// console.log(showFields(user));
+
+// //v3 type guards
+// abstract class Guy {
+//   constructor(public name: string) {}
+// }
+
+// class Good extends Guy {
+//   advice() {
+//     console.log('advice');
+//   }
+// }
+// class Bad extends Guy {
+//   insult() {
+//     console.log('insult');
+//   }
+// }
+
+// const good = new Good('Dir');
+// const bad = new Bad('Lek');
+
+// function guys(user: Guy) {
+//   if (user instanceof Good) {
+//     user.advice();
+//   }
+//   if (user instanceof Bad) {
+//     user.insult();
+//   }
+// }
+// console.log(guys(good));
+
+//v4 type casting
+
+// const input = <HTMLInputElement>document.getElementById('inputEmail');
+
+// const input = document.getElementById('inputEmail') as HTMLInputElement;
+
+// const input = document.getElementById('inputEmail');
+
+// if (input) {
+//   (input as HTMLInputElement).value;
+//   const newInput = input as HTMLInputElement;
+//   newInput.value;
+// }
+
+//v5
+// interface Person {
+//   name: string;
+//   age: number;
+//   // [x: string]: string | number;
+//   [x: string]: any;
+// }
+
+// const user: Person = {
+//   age: 30,
+//   name: 'Alic',
+//   gender: 'Man',
+//   country: 'Ukraine',
+// };
+
+//v8 function overloads
+// function add(a: string | number, b: string | number) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+
+// v9 Generics
+// let arr: Array<string | number>;
+// arr = ['string', 1];
+
+// const promise: Promise<string> = new Promise(resolve => {
+//   resolve('string');
+// });
+// promise.then(data => {});
+
+// (() => {
+//   async function promiseFoo(): Promise<string> {
+//     return 'String2';
+//   }
+//   promiseFoo().then(data => {});
+// })();
+
+//v10
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign({}, objA, objB);
+// }
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign({}, objA, objB);
+// }
+
+// type Person = {
+//   name: string;
+// };
+// type AdditionalFields = {
+//   age: number;
+// };
+
+// const toMerge1 = {
+//   name: 'Max',
+// };
+// const toMerge2 = {
+//   age: 21,
+// };
+
+// // const merged = merge(toMerge1, toMerge2);
+// // merged.name; //Property 'name' does not exist on type 'object'
+
+// const merged = merge<Person, AdditionalFields>(toMerge1, toMerge2);
+// merged.name;
+
+//v11
+// function merge<T extends object, U extends object>(objA: T, objB: U) {
+//   return Object.assign({}, objA, objB);
+// }
+
+// const toMerge1 = {
+//   name: 'Max',
+// };
+// const toMerge2 = {
+//   age: 21,
+// };
+
+// const merged = merge(toMerge1, toMerge2);
+
+// merged.name;
+// interface ILength {
+//   length: number;
+// }
+
+// function getLength<T extends ILength>(str: T) {
+//   return str.length;
+// }
+
+// console.log(getLength('hsdfhgsdhf shfhsdgfshfgshdgf'));
+
+// function extractValue<T extends object, U extends keyof T>(obj: T, key: U) {
+//   return obj[key];
+// }
+
+// const field = extractValue({ name: 'Ali' }, 'name');
+// console.log('field: ', field);
+
+//v13
+// class DataStore<T> {
+//   private data: T[] = [];
+
+//   addItems(item: T): void {
+//     this.data.push(item);
+//   }
+
+//   getItems(): T[] {
+//     return this.data;
+//   }
+// }
+
+// interface IPerson2 {
+//   name: string;
+// }
+
+// const storeUsers = new DataStore<IPerson2>();
+
+// storeUsers.addItems({
+//   name: 'Max',
+// });
+// storeUsers.addItems({
+//   name: 'Bigl',
+// });
+// console.log(storeUsers.getItems());
+
+// const ageStoreUsers = new DataStore<number>();
+// ageStoreUsers.addItems(21);
+// ageStoreUsers.addItems(35);
+
+// //v14 Partial
+// interface IUser {
+//   name: string;
+//   age: number;
+// }
+
+// function createPerson(name: string): IUser {
+//   const person: Partial<IUser> = {
+//     name,
+//   };
+//   // person.age = 21;
+//   return person as IUser;
+// }
+
+// function getPromise(): Promise<Array<string | number>> {
+//   return new Promise(resolve => {
+//     resolve(['Text', 50]);
+//   });
+// }
+
+// getPromise().then(data => {
+//   console.log(data);
+// });
+// console.log(getPromise());
+
+// type AllType = {
+//   name: string;
+//   position: number;
+//   color: string;
+//   weight: number;
+// };
+
+// function compare(
+//   top: Pick<AllType, 'name' | 'color'>,
+//   bottom: Pick<AllType, 'position' | 'weight'>
+// ): AllType {
+//   return {
+//     name: top.name,
+//     color: top.color,
+//     position: bottom.position,
+//     weight: bottom.weight,
+//   };
+// }
+
+// function merge<T extends object, U extends object>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
+
+// class Component<T> {
+//   constructor(public props: T) {}
+// }
+
+// interface IProps {
+//   title: string;
+// }
+
+// class Page extends Component<IProps> {
+//   pageInfo() {
+//     console.log(this.props.title);
+//   }
+// }
+
+////some example
+
+// const obj: { func(): void; x: number } = {
+//   x: 18,
+//   func(): void {
+//     setTimeout(function (): void {
+//       console.log(this.x);
+//     }, 1000);
+//   },
+// };
+// console.log(obj.func()); //undefined => При використанні функції зворотного виклику setTimeout контекст виконання (this) всередині функції не буде покликатись на об'єкт obj.
