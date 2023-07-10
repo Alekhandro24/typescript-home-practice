a9c26f9aa21eebe8be6f9f4a574c1a2ac5a223d2
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-10 15:59:17 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -787,3 +787,262 @@ interface User {
 //   },
 // };
 // console.log(obj.func()); //undefined => При використанні функції зворотного виклику setTimeout контекст виконання (this) всередині функції не буде покликатись на об'єкт obj.
+
+//V 6.1 декоратори
+// function Logger(logString: string) {
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+// function AddProperty() {
+//   return function (constructor: Function) {
+//     console.log('AddProperty');
+//     constructor.prototype.modify = true;
+//   };
+// }
+// @Logger('LOGGIN-CONTROLLER')
+// @AddProperty()
+// class Controller {
+//   public id = 1;
+//   public modify?: boolean;
+// }
+
+// const controller = new Controller();
+// console.log('Is modify?', controller.modify);
+
+//v 6.2
+// interface IDecoration {
+//   parent: string;
+//   templete: string;
+// }
+
+// function ControllerDecoration(config: IDecoration) {
+//   return function (constructor: any) {
+//     const current = new constructor(); // створили об*єкт і це погано
+
+//     const getParent = document.getElementById(config.parent);
+//     const createElement = document.createElement(config.templete);
+//     createElement.innerHTML = current.content;
+//     constructor.prototype.element = createElement;
+//     constructor.prototype.parent = getParent;
+//     getParent?.appendChild(createElement);
+//   };
+// }
+// @ControllerDecoration({
+//   parent: 'app',
+//   templete: 'H1',
+// })
+// class Controller {
+//   public content = 'My controller';
+// }
+
+//-2
+// interface IDecoration {
+//   parent: string;
+//   templete: string;
+// }
+
+// function ControllerDecoration(config: IDecoration) {
+//   return function <T extends { new (...arg: any[]): { content: string } }>(
+//     originalConstructor: T
+//   ) {
+//     return class extends originalConstructor {
+//       private element: HTMLElement;
+//       private parent: HTMLElement | null;
+
+//       constructor(...args: any[]) {
+//         super(...args);
+//         this.parent = document.getElementById(config.parent);
+//         this.element = document.createElement(config.templete);
+
+//         this.element.innerHTML = this.content;
+//         this.parent?.appendChild(this.element);
+//       }
+//     };
+//   };
+// }
+// @ControllerDecoration({
+//   parent: 'app',
+//   templete: 'h3',
+// })
+// class Controller {
+//   public content = 'My controller';
+// }
+
+// const controller1 = new Controller();
+// const controller2 = new Controller();
+// const controller3 = new Controller();
+
+//v6 3 дeкоратори методів
+// function ShowParams(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log('target', target);
+//   console.log('name', name);
+//   console.log('descriptor', descriptor);
+// }
+
+// function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+//   const method = descriptor.value as Function;
+
+//   return {
+//     configurable: true,
+//     enumerable: false,
+//     get() {
+//       return method.bind(this);
+//     },
+//   };
+// }
+
+// class Notifier {
+//   public content = 'This content in this';
+//   @ShowParams
+//   @AutoBind
+//   showMessage() {
+//     console.log(this.content);
+//   }
+// }
+
+// const notifier = new Notifier();
+// const showMessage = notifier.showMessage;
+// notifier.showMessage();
+// showMessage();
+
+// 3 - 2
+
+// function AddTax(taxParsent: number) {
+//   return function (_: any, _2: string, descriptor: PropertyDescriptor) {
+//     const method = descriptor.value as Function;
+
+//     return {
+//       configurable: true,
+//       enumerable: false,
+//       get() {
+//         return (...args: any[]) => {
+//           const result = method.apply(this, args);
+//           return result + (result / 100) * taxParsent;
+//         };
+//       },
+//     };
+//   };
+// }
+
+// class Payment {
+//   @AddTax(20)
+//   pay(money: number) {
+//     return money;
+//   }
+// }
+
+// const payment = new Payment();
+// console.log(payment.pay(1500));
+
+//v 6.4 декоратори параметрів
+
+// function CheckEmail(target: any, nameMethod: string, position: number) {
+//   if (!target[nameMethod].validation) {
+//     target[nameMethod].validation = {};
+//   }
+//   Object.assign(target[nameMethod].validation, {
+//     [position]: (value: string) => {
+//       if (value.includes('@')) {
+//         return value;
+//       }
+//       throw new Error('Not valid email');
+//     },
+//   });
+// }
+
+// function Validation(
+//   _: any,
+//   _2: string,
+//   descriptor: PropertyDescriptor
+// ): PropertyDescriptor {
+//   const method = descriptor.value;
+//   return {
+//     configurable: true,
+//     enumerable: false,
+//     get() {
+//       return (...args: any[]) => {
+//         if (method.validation) {
+//           args.forEach((item, index) => {
+//             if (method.validation[index]) {
+//               args[index] = method.validation[index](item);
+//             }
+//           });
+//         }
+//         return method.apply(this, args);
+//       };
+//     },
+//   };
+// }
+// class Person {
+//   @Validation
+//   setEmail(@CheckEmail email: string) {
+//     console.log('email', email);
+//   }
+// }
+
+// const person = new Person();
+
+// person.setEmail('test@test.com'); // ok!
+
+//v 6.5 декоратори свойств
+interface ValidationConfig {
+  [prop: string]: {
+    [validationProp: string]: string[];
+  };
+}
+const registeredValidation: ValidationConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidation[target.constructor.name] = {
+    ...registeredValidation[target.constructor.name],
+    [propName]: ['required'],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidation[target.constructor.name] = {
+    ...registeredValidation[target.constructor.name],
+    [propName]: ['positive'],
+  };
+}
+
+function validation(obj: any) {
+  const objectValidation = registeredValidation[obj.constructor.name];
+  if (!objectValidation) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objectValidation) {
+    for (const validProp of objectValidation[prop]) {
+      switch (validProp) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+class Person {
+  @Required
+  public name: string;
+  @PositiveNumber
+  public age: number;
+  constructor(n: string, a: number) {
+    this.name = n;
+    this.age = a;
+  }
+}
+
+const person = new Person('Ali', 21);
+if (!validation(person)) {
+  console.log('Not valid');
+} else {
+  console.log(`Valid ${JSON.stringify(person)}`);
+}
